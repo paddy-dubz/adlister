deb0332ddf1bea29b88fff730b51f7c752014b0f
Patrick Woolard <patrick.k.woolard@gmail.com>
2016-12-15 09:56:35 -0600

    Started session exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -14,7 +15,11 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
+
+
         if (username.equals("admin") && password.equals("password")) {
+            HttpSession session=request.getSession();
+            session.setAttribute("username", username);
             response.sendRedirect("profile.jsp");
         } else {
             doGet(request, response);
--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,28 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+
+        HttpSession session=request.getSession(false);
+
+        if(session!=null){
+            String username = (String)session.getAttribute("user");
+            out.print("Hello, "+username+" Welcome to Profile");
+            request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        }
+        else{
+            out.print("Please login first");
+            request.getRequestDispatcher("/login.jsp").include(request, response);
+        }
+
     }
 }
