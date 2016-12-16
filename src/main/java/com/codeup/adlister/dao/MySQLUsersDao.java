81ce6f9f4ab768816737be7dc5b92a31ab1fd286
Patrick Woolard <patrick.k.woolard@gmail.com>
2016-12-16 15:38:55 -0600

    Updated user-input exercise

---
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -0,0 +1,64 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.controllers.Config;
+import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.List;
+
+/**
+ * Created by Patrick on 12/16/16.
+ */
+public class MySQLUsersDao implements Users {
+    private Connection connection = null;
+
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+    public List<User> all() {
+        try {
+            PreparedStatement stmt = connection.prepareStatement("SELECT * from users");
+            ResultSet rs = stmt.executeQuery();
+            return createAdsFromResults(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving all ads.", e);
+        }
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public Long insert(User user) {
+        try {
+            PreparedStatement stmt = connection.prepareStatement(
+                    "INSERT INTO users (id, username, email, password) VALUES (?, ? , ? , ?);",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            stmt.setLong(1, user.getId());
+            stmt.setString(2, user.getUsername());
+            stmt.setString(3, user.getEmail());
+            stmt.setString(4, user.getPassword());
+
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new user.", e);
+        }
+    }
+}
---
+++ src/main/webapp/WEB-INF/register.jsp
@@ -0,0 +1,34 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Patrick
+  Date: 12/16/16
+  Time: 3:23 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<div class="container">
+    <h1>Create a new Ad</h1>
+    <form action="/ads/register" method="post">
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input id="username" name="username" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <textarea id="email" name="email" class="form-control" type="text"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input id="password" name="password" class="form-control" type="password">
+        </div>
+        <input type="submit" class="btn btn-block btn-primary">
+    </form>
+</div>
+</body>
+</html>
+
